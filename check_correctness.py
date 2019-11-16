fb5ae8fcb9649bb17ad806933b3195410c8d9dfd
Peter Schmidt-Nielsen <schmidtnielsenpeter@gmail.com>
2019-11-15 18:18:38 -0800

    Adding some options for performance testing.

--- check_correctness.py
+++ check_correctness.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-import subprocess, glob, json, sys, argparse
+import subprocess, glob, json, sys, argparse, time
 
 devnull = open("/dev/null")
 retcode_to_string = {10: "sat", 20: "unsat"}
@@ -20,16 +20,20 @@ def populate_answers():
 
 	print("Satisfiable fraction:", sum(a == "sat" for a in mapping.values()) / len(mapping))
 
-def test_sat_solver(solver_path):
+def test_sat_solver(args):
 	with open("test_instances/answers", "r") as f:
 		mapping = json.load(f)
 
 	any_failures = False
 	for path in sorted(glob.glob("test_instances/*.dimacs")):
+
 		var_count = int(path.split("vars")[1].split("_")[0])
+		if not ((100 <= var_count <= 120 and args.medium_sized) or (var_count <= 100 and not args.medium_sized)):
+			continue
+
 		print(path, end=" ")
 		sys.stdout.flush()
-		retcode = subprocess.call([solver_path, path], stdout=devnull)	
+		retcode = subprocess.call([args.solver, path], stdout=devnull)	
 		if retcode_to_string[retcode] == mapping[path]:
 			print("pass")
 		else:
@@ -41,6 +45,7 @@ def test_sat_solver(solver_path):
 parser = argparse.ArgumentParser()
 parser.add_argument("--populate-answers", action="store_true", help="Run minisat on all of the test instances, and cache the answers.")
 parser.add_argument("--solver", default="./scowsat", type=str, help="Path to the solver to test.")
+parser.add_argument("--medium-sized", action="store_true", help="Run on just the 100 variable and 120 variable instances, then exit. For performance testing.")
 
 if __name__ == "__main__":
 	args = parser.parse_args()
@@ -51,5 +56,8 @@ if __name__ == "__main__":
 		exit()
 
 	print("Testing", args.solver)
-	test_sat_solver(args.solver)
+	start = time.time()
+	test_sat_solver(args)
+	elapsed = time.time() - start
+	print("Time: %.2fms" % (1e3 * elapsed,))
 
